ba47ea1bc8d900906668b4bbefc5ebfc787b6ae4
eyupcomez <eyupcomez46@gmail.com>
2026-02-14 00:36:07 +0300

    mesaj

--- bankAccount/src/Balance.java
+++ bankAccount/src/Balance.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Balance {
+    public double balance = 0;
+    public void add (){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How much do you want to add? ");
+        double amount = scanner.nextDouble();
+
+        if(amount> 0){
+            balance += amount;
+        }else{
+            System.out.println("Amount must be higher than 0!");
+        }
+        System.out.println("New balance: " + balance);
+
+    }
+
 }
